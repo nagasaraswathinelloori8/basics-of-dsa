6b1fa801c3be5c82349572a494a3e94f0f6969d6
nagasaraswathinelloori8 <nagasaraswathinelloori8@gmail.com>
2024-02-01 10:50:56 +0530

    merge

--- DevideAndConquer/Mergesort.java
+++ DevideAndConquer/Mergesort.java
@@ -1,7 +1,11 @@
 
 public class Mergesort {
-    
+    public static void Getresult(int [] arr ,int si,int ei){
+
+ System.out.println("hi");
+    }
     public static void main(String[] args) {
-        System.out.println("hi nanna");
+        int arr[]={6,3,9,5,2,8,};
+       Getresult(arr ,0,arr.length-1);
     }
 }
